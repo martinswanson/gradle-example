321b17cc2e0f2cc1f0c68e003a8e027963a85bfd
atomic-wire <info@atomicwire.io>
2019-07-27 22:50:52 +0100

    Initial commit

---
+++ src/main/java/io/example/Main.java
@@ -0,0 +1,13 @@
+package io.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static Logger LOG = LoggerFactory.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOG.debug("Hello World!");
+    }
+}
